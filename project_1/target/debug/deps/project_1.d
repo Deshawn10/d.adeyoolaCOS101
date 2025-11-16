4227cd9ac41b1240f7b62dee55457c20460ea965
Deshawn10 <olufemi.adeyoola@pau.edu.ng>
2025-11-17 00:15:41 +0100

    Delete project_1 directory

--- project_1/target/debug/deps/project_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\desha\Videos\d.adeyoolaCOS101\project_1\target\debug\deps\project_1.d: src\main.rs
-
-C:\Users\desha\Videos\d.adeyoolaCOS101\project_1\target\debug\deps\project_1.exe: src\main.rs
-
-src\main.rs:
--- project_1/target/debug/project_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\desha\Videos\d.adeyoolaCOS101\project_1\target\debug\project_1.exe: C:\Users\desha\Videos\d.adeyoolaCOS101\project_1\src\main.rs
