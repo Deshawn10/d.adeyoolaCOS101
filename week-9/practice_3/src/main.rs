a2b0cc6525944d839d8996de5f98a77a60fe6c41
Deshawn Adeyoola <olufemi.adeyoola.pau.edu.ng>
2025-11-26 15:44:22 +0100

    Program to remove created file (data.txt) created in the code

--- week-9/practice_3/src/main.rs
+++ week-9/practice_3/src/main.rs
@@ -1,3 +1,16 @@
+use std::io::Write;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+ let announce = "Week 9 - Rust File Input & Output \n";
+    let dept = "Department of Computer Science";
+
+    let mut file = std::fs::File::create("data.txt").expect("create failed");
+    file.write_all("Welcome to Rust Programming\n"
+    .as_bytes()).expect("Write failed");
+    file.write_all(announce.as_bytes()).expect("write failed");
+    file.write_all(dept.as_bytes()).expect("write failed");
+
+    fs::remove_file("data.txt").expect("could not remove file");
+    println!("file has been removed");
 }
